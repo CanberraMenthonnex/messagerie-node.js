aae19b27d0c33f46fd5b4a21c16b1551ac61f92d
Mathieu <mathieu.raimbault@hotmail.com>
2022-01-05 16:15:50 +0100

    :sparkles: add message read operation

--- routes/conversations.js
+++ routes/conversations.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { createConversation, getConversations, deleteConversation, updateConversation } = require("../controllers/conversations");
+const { getMessages } = require("../controllers/messages");
 const {auth} = require("../middlewares/auth")
 const router = express.Router()
 
@@ -12,5 +13,7 @@ router.put("/", updateConversation)
 router.get("/", getConversations)
 
 router.delete("/", deleteConversation)
+
+router.use("/:conversationId/messages", getMessages)
 
 module.exports = router
